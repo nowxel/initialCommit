876244c83d6ee2378e5c59fd52aaa01e921b075f
nowxel <nowxel@gmail.com>
2021-01-18 20:07:14 +0200

    Fourth commit

--- lib/test.dart
+++ lib/test.dart
@@ -1,4 +1,17 @@
-void main(){
-  //comment
-  print('hello');
+void main() {
+  var name = 'Andrea';
+  var age = 25;
+  var height = 1.84;
+
+  describe(name, age, height);
+  describe('Matt', 27, 1.76);
+
 }
+  void describe(String name, int age, double height) {
+    print("Heelo, I am $name");
+    print("My name has ${name.length} letters");
+    print("I'm $age years old");
+    print("I'm $height meters tall");
+
+  }
+
